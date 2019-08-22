ba5bd655ef1b87f0c2fb09a572d2eea6105efd84
sloothes <sloothes.com@gmail.com>
2019-08-22 06:21:58 +0300

    Update gws-sw.js

--- gws-sw.js
+++ gws-sw.js
@@ -1,1 +1,42 @@
 //  google webspace search sw.js
+
+    self.version = 3.5;
+    var debugMode = true;
+
+    self.importScripts(
+        "/js/Objectid.js",
+        "/js/zangodb.min.js",
+    );
+
+
+
+
+
+
+
+
+
+
+
+    self.addEventListener("install", function(e){
+
+        self.skipWaiting();
+
+    });
+
+    self.addEventListener("activate", function(e){
+
+        self.clients.claim();
+
+    });
+
+    function unistall(){
+        self.registration.unregister().then(function(){
+            return self.clients.matchAll();
+        }).then(function(clients) {
+            clients.forEach(function(client){
+                client.navigate(client.url);  // it will re-install on reload!
+                console.log(`service worker unistalled from client "${client.url}"`);
+            });
+        });
+    }
